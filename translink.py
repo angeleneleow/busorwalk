a586f37e33412c354d72ce6cb4578754c55d6d9a
Angelene Leow <angeleneleowubc@gmail.com>
2018-06-17 23:33:26 -0700

    Make helper function for GET requests

--- translink.py
+++ translink.py
@@ -5,6 +5,7 @@ import json
 BASE_URL = 'http://api.translink.ca/rttiapi/v1'
 API_ARG = '?apikey='
 
+
 class API:
     '''
     Class API contains an interface to the translink API
@@ -13,6 +14,12 @@ class API:
 
     def __init__(self, key):
         self._key = key
+
+    def _get(self, url):
+        req = urllib2.Request(url, None, {
+            'Content-Type': 'application/json'})
+        contents = urllib2.urlopen(req).read()
+        return json.loads(contents)
 
     def bus_stop_request(self, stopNumber, endpoint):
         '''
@@ -25,10 +32,7 @@ class API:
         requestURL = base + '/' + stopNumber + endpoint + API_ARG + self._key
 
         # Make request
-        req = urllib2.Request(requestURL, None, {
-                            'Content-Type': 'application/json'})
-        contents = urllib2.urlopen(req).read()
-        contents = json.loads(contents)
+        contents = self._get(requestURL)
 
         # Return result
         return contents
@@ -42,10 +46,7 @@ class API:
         requestURL = base + API_ARG + self._key
 
         # Make request
-        req = urllib2.Request(requestURL, None, {
-                            'Content-Type': 'application/json'})
-        contents = urllib2.urlopen(req).read()
-        contents = json.loads(contents)
+        contents = self._get(requestURL)
 
         # Return result
         return contents
